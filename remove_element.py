49b0d2b0933effb766f568b881ed38d834726594
Chako <ryland0000@gmail.com>
2023-08-20 21:13:47 -0700

    remove_element finished

--- remove_element.py
+++ remove_element.py
@@ -1,6 +1,13 @@
 class Solution:
-    def removeElement(self, nums: [int], val: int) -> int:
-        pass
+    def removeElement(self, nums, val):
+        while True:
+            if nums.count(val) != 0:
+                nums.remove(val)
+            else:
+                break
+        k = len(nums)
+        print(k, nums)
+        return k
 
 """
 Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
@@ -58,19 +65,19 @@ Constraints:
 Remember the acronym UPS check
 
 Step One (Understand). We have to understand the problem.
-1. What is the unknown?
-2. What are the data inputs?
-3. Can you restate the problem in your own words?
-4. Can the unknown be determined from the data inputs?
-5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory? 
-6. How should I label important pieces of data input that are a part of the problem?
+1. What is the unknown? remove all occurrences of val in nums in-place and return the number of elements "k" left in nums
+2. What are the data inputs? integer list nums and integer val
+3. Can you restate the problem in your own words? find all val and remove it from nums
+4. Can the unknown be determined from the data inputs? yes
+5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory? yes
+6. How should I label important pieces of data input that are a part of the problem? nums, val, k
 7. Draw a figure. Introduce suitable notation.
-
+s
 Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found. 
 8.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem? 
-9.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method? 
+9.   Do you know a coding strategy that could be useful? for in loop Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method? 
 10. Start with a simple example. Could you restate the problem? Could you restate it still differently? 
-11. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs? 
+11. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs? negative val
 12. Progress to more complex examples. What is your updated plan here to find a solution?
 13. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
 
